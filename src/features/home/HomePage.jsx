1ecb6a8d7544848a95d9f9ac8af53958a537f6fb
Ankit Chourasia <iankit.chourasia@gmail.com>
2026-07-21 22:04:38 +0530

    OutcomeCards disabled.

--- src/features/home/HomePage.jsx
+++ src/features/home/HomePage.jsx
@@ -22,7 +22,7 @@ export default function HomePage() {
       <StatsBand />
       <CertifyingBodies />
       <DomainGrid />
-      <OutcomeCards />
+      {/* <OutcomeCards /> */}
       <FeaturedCourses />
       <HowItWorks />
       <Testimonials />
